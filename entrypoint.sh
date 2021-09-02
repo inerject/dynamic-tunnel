db46139debace26eb92027bb8b66277d996db8ee
inerject <kumbalup@gmail.com>
2021-09-02 11:24:03 +0300

    add IDENTITY_FILE optional parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
+SSH_SRC_DATA=/root/ssh
+SSH_DEST_DATA=/root/.ssh
+
 if [ -z $LOCAL_HOST ]; then
   LOCAL_HOST=0.0.0.0
 fi
 
 SSH_CMD_OPTIONS="-N -D ${LOCAL_HOST}:3000"
+
 if [ ! -z $DEST_PORT ]; then
   SSH_CMD_OPTIONS="-p ${DEST_PORT} ${SSH_CMD_OPTIONS}"
 fi
 
-rm -rf /root/.ssh \
-  && mkdir /root/.ssh \
-  && cp -R /root/ssh/* /root/.ssh/ \
-  && chmod -R 600 /root/.ssh/* \
+if [ ! -z $IDENTITY_FILE ]; then
+  SSH_CMD_OPTIONS="-i ${SSH_DEST_DATA}/${IDENTITY_FILE} ${SSH_CMD_OPTIONS}"
+fi
+
+rm -rf ${SSH_DEST_DATA} \
+  && mkdir ${SSH_DEST_DATA} \
+  && cp -R ${SSH_SRC_DATA}/* ${SSH_DEST_DATA}/ \
+  && chmod -R 600 ${SSH_DEST_DATA}/* \
   && ssh $SSH_CMD_OPTIONS $DEST_USER@$DEST_HOST \
   && while true; do sleep 50; done;
